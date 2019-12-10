533183e6dec7a977f4ec85bab44e33d68cb94f25
msicsic <msicsic@yahoo.Com>
2019-12-10 21:25:30 +0100

    feature deposit: pending: UT deposit 100 and 200

--- src/test/kotlin/org/msi/kata/bankAccountKt/AccountShould.kt
+++ src/test/kotlin/org/msi/kata/bankAccountKt/AccountShould.kt
@@ -1,5 +1,6 @@
 package org.msi.kata.bankAccountKt
 
+import com.nhaarman.mockitokotlin2.inOrder
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import org.junit.jupiter.api.Test
@@ -19,6 +20,25 @@ internal class AccountShould {
 
         // THEN
         verify(history).add(Operation(Money(100), now))
+    }
+
+    @Test
+    fun `deposit 100 and 200 added in history`() {
+        // GIVEN
+        val history = mock<OperationHistory>()
+        val account = Account(history)
+        val now = LocalDateTime.now()
+
+        // WHEN
+        account.deposit(Money(100), now)
+        account.deposit(Money(200), now)
+
+        // THEN
+        inOrder(history) {
+            verify(history).add(Operation(Money(100), now))
+            verify(history).add(Operation(Money(200), now))
+        }
+
     }
 
 }
